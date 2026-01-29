800565d20276b9d8c27ed2a6c9115848ea28d590
Abhi <abhangabhaishek99200@gmail.com>
2026-01-29 13:10:50 +0530

    Solved the bug

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abhi_portfolio.settings')
+# 👇 CHANGE THIS to your actual Django project folder name
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "abhi_portfolio.settings"
+)
 
-app = get_wsgi_application()
+application = get_wsgi_application()
