1d56ca1e461518f9a01dd55e9c314a45337cce90
Abhi <abhangabhaishek99200@gmail.com>
2026-01-29 12:48:10 +0530

    Portfolio is Updated

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abhi_portfolio.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abhi_portfolio.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+app = application
