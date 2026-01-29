d24e643c609ceeaef902f8908db08251beb800b5
Abhi <abhangabhaishek99200@gmail.com>
2026-01-29 14:16:40 +0530

    updated urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,11 @@
 from portfolio import views
 from django_distill import distill_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     distill_path('', views.get_index, name='home'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
